98abbd81433378e911349a0793d6b827e6bf5542
Victor Kane <victorkane@gmail.com>
2022-11-14 13:22:23 -0300

    regular nuxt page plus two markdown content pages with navigation

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello, world</h1>
+    <ul>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/the-content">Content</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
